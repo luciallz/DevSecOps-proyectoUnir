96e827b41749a498c4c21ce0ba73f199b3cd65c5
kali2 <kali2@kali2.home>
2025-06-19 22:26:30 +0200

    d

--- src/flask/app.py
+++ src/flask/app.py
@@ -68,6 +68,9 @@ app = Flask(__name__)
 def home():
     return "¡Hola desde Flask!"
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
 T_shell_context_processor = t.TypeVar(
     "T_shell_context_processor", bound=ft.ShellContextProcessorCallable
 )
